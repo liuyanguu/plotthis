256b16cf02484f9d87a1256e01d7b023fe883e57
liuyanguu <lyhello@gmail.com>
2019-08-09 21:34:16 -0400

    add dilute and more files

--- R/plotthis.R
+++ R/plotthis.R
@@ -8,36 +8,43 @@
 #' @param y y
 #' @param size0 point size, default to 1 of nobs<1000, 0.4 if nobs>1000
 #' @param alpha0 alpha of point
-#' @param dilute a number or logical, dafault to TRUE, will plot \code{nrow(data_long)/dilute} data. For example, if dilute = 5 will plot 1/5 of the data.
-#' @param add_hist optional to add marginal histogram using `ggExtra::ggMarginal` but notice
+#' @param dilute a number or logical, dafault to TRUE,
+#'  will plot \code{nrow(data_long)/dilute} data. For example,
+#'  if dilute = 5 will plot 1/5 of the data.
+#'  if dilute = TRUE will plot half of the data.
+#' @param add_hist optional to add marginal histogram
+#' using `ggExtra::ggMarginal` but notice
 #' if add histogram, what is returned is no longer a ggplot2 object
 #'
 #' @return ggplot2 object if `add_hist = FALSE`
 #' @examples
-#' scatter.plot.simple(data = datam1_terra, x = "col_water_cm", y = "Column_WV")
+#' scatter.plot.simple(data = shap_score, x = "dayint", y = "AOT_Uncertainty")
 #'
 #' @export scatter.plot.simple
 #'
-scatter.plot.simple <-  function(data, x, y, size0 = 0.1, alpha0 = 0.3,
+scatter.plot.simple <-  function(data, x, y, size0 = 0.2, alpha0 = 0.3,
                                  dilute = FALSE,
                                  add_hist = TRUE){
   set.seed(1234)
   if (is.null(dilute)) dilute = FALSE
   if (dilute!=0){
-    dilute <- ceiling(min(nrow(data0)/10, abs(as.numeric(dilute)))) # not allowed to dilute to fewer than 10 obs/feature
+    dilute <- ceiling(min(nrow(data)/10, abs(as.numeric(dilute))))
+    # not allowed to dilute to fewer than 10 obs/feature
     set.seed(1234)
-    data0 <- data0[sample(nrow(data0), min(nrow(data0)/dilute, 1500))] # dilute
+    data <- data[sample(nrow(data), min(nrow(data)/dilute, nrow(data)/2))]
   }
-  if (is.null(size0)) size0 <- if(nrow(data0)<1000L) 1 else 0.4
+  if (is.null(size0)) size0 <- if(nrow(data)<1000L) 1 else 0.2
 
   plot0 <- ggplot(data = data, aes(x = data[[x]], y = data[[y]]))+
     geom_point(size = size0, alpha = alpha0) +
     # geom_density_2d(aes(fill = ..level..), geom = "polygon") +
     labs(x = x, y = y) +
     theme_bw()
+
   # add histogram by default
   if(add_hist){
-    plot0 <- ggExtra::ggMarginal(plot0, type = "histogram", bins = 50, size = 10, color="white")
+    plot0 <- ggExtra::ggMarginal(plot0, type = "histogram",
+                                 bins = 50, size = 10, color="white")
   }
   return(plot0)
 }
@@ -47,41 +54,52 @@ scatter.plot.simple <-  function(data, x, y, size0 = 0.1, alpha0 = 0.3,
 #' @import data.table
 #' @importFrom BBmisc capitalizeStrings
 #'
-#' @param data dataset
-#' @param x x
-#' @param y y
-#' @param dilute a number or logical, dafault to TRUE, will plot \code{nrow(data_long)/dilute} data. For example, if dilute = 5 will plot 1/5 of the data.
+#' @inheritParams scatter.plot.simple
 #' @param add_abline default to FALSE, add a diagonal line
-#' @param add_hist optional to add marginal histogram using `ggExtra::ggMarginal` but notice
+#' `ggExtra::ggMarginal` but notice
 #' if add histogram, what is returned is no longer a ggplot2 object
-#'
 #' @return ggplot2 object if `add_hist = FALSE`
 #' @examples
-#' scatter.plot.diagonal(data = datam1_terra, x = "col_water_cm", y = "Column_WV", add_abline = T)
+#' scatter.plot.diagonal(data = iris, x = "Sepal.Length", y = "Petal.Length")
 #'
 #' @export scatter.plot.diagonal
 #'
-scatter.plot.diagonal <- function(data, x, y, dilute = F, add_abline = F, add_hist = T){
-  # with diagonal line and R2
-  if (x == 'dayint'){
-    setDT(data)[, dayint:= as.Date(data[,dayint], format = "%Y-%m-%d",origin = "1970-01-01")]
+scatter.plot.diagonal <- function(data, x, y,
+                                  size0 = 0.2, alpha0 = 0.3,
+                                  dilute = FALSE,
+                                  add_abline = FALSE,
+                                  add_hist = TRUE){
+  set.seed(1234)
+  if (is.null(dilute)) dilute = FALSE
+  if (dilute!=0){
+    dilute <- ceiling(min(nrow(data)/10, abs(as.numeric(dilute))))
+    # not allowed to dilute to fewer than 10 obs/feature
+    set.seed(1234)
+    data <- data[sample(nrow(data), min(nrow(data)/dilute, nrow(data)/2))]
   }
-  # core plot
+  if (is.null(size0)) size0 <- if(nrow(data)<1000L) 1 else 0.2
+
   plot1 <-  ggplot(data = data, aes(x = data[[x]], y = data[[y]]))+
-    geom_point(size = 0.1, alpha = 0.3) +
+    geom_point(size = size0, alpha = alpha0) +
     theme_bw() +
     geom_smooth(method = 'lm') +
     labs(x = BBmisc::capitalizeStrings(x), y = BBmisc::capitalizeStrings(y)) +
     ggpubr::stat_cor(method = "pearson")
 
+
+
   if(add_abline){
     plot1 <- plot1 + geom_abline(intercept =0 , slope = 1, color = "grey")
-
   }
-  cat("R2 is", summary(lm(data[[y]]~data[[x]], na.action = na.omit))$r.squared,"\n")
+
+  print(paste("R2 is",
+              signif(summary(lm(data[[y]]~data[[x]],
+                                na.action = na.omit))$r.squared, 4),
+              "."))
   # customize labels
   if(add_hist){
-    plot1 <- ggExtra::ggMarginal(plot1, type = "histogram", bins = 50, size = 10, color="white")
+    plot1 <- ggExtra::ggMarginal(plot1, type = "histogram",
+                                 bins = 50, size = 10, color="white")
   }
   return(plot1)
 }
@@ -104,9 +122,9 @@ ggmap.plot.us <- function(data0){
 }
 
 
-#' us map with fill = size0.
+#' The US map with fill = size0.
+#' @importFrom Hmisc capitalize
 #'
-
 ggmap.plot.us.size <- function(data0 = aeronetdt, size0 = "Ndays", sat0 = sat, NEMIA = F){
   # optional to print NEMIA alone
   NE_list <- c("maine", "new hampshire", "vermont",
